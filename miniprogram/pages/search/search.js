b9b9a6727a5356bdfd7b8b24466d48fa5bca9b8e
Wellessun <3200102339@zju.edu.cn>
2022-02-16 00:24:11 +0800

    Search页的头部和搜索框初步

--- miniprogram/pages/search/search.js
+++ miniprogram/pages/search/search.js
@@ -1,6 +1,9 @@
 Page({
     // 页面数据
     data : {
+        // Swiper相关的data
+        swiperHidden: false,
+        information: [1, 2, 3]
 
     },
     
@@ -10,9 +13,13 @@ Page({
         if (typeof this.getTabBar === 'function' && this.getTabBar()) {
             this.getTabBar().setData({
                 selected: 0,
+                page: this
             });
         }
-
+        
+        this.setData({
+            page: 0,
+          });
     },
 
 
