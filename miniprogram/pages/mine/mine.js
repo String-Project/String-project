85019eb5f8c8e9981fd3dbbe91e0fe9107f8b71c
Wellessun <3200102339@zju.edu.cn>
2022-03-11 23:56:33 +0800

    修复了一个TabBar的BUG

--- miniprogram/pages/mine/mine.js
+++ miniprogram/pages/mine/mine.js
@@ -25,13 +25,13 @@ Page({
 
         if (typeof this.getTabBar === 'function' && this.getTabBar()) {
             this.getTabBar().setData({
-                selected: 0,
+                selected: 1,
                 page: this
             });
         }
         
         this.setData({
-            page: 0,
+            page: 1,
           });
     },
 
