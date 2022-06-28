bed54d606eac74bafbd776c8eb35181546e5b376
ArgoC <193551011@qq.com>
2022-06-28 16:05:53 +0800

    经历页面数据库交互基本完成

--- miniprogram/pages/information/mainpage/mainpage.js
+++ miniprogram/pages/information/mainpage/mainpage.js
@@ -1,3 +1,4 @@
+//const { userInfo } = require("os");
 
 var App=getApp()
 const db = wx.cloud.database(); 
@@ -40,6 +41,22 @@ Page({
         that.setData({
             userInfo: App.globalData.userInfo,
         });
+
+        //获取searchResult
+        db.collection('Info').where({
+            _openid : App.globalData.userInfo._openid //是同一用户的信息才会被查询，这里为啥直接用userInfo会出问题？
+          }).get({
+            success: res => {   
+              that.setData({
+                searchResult: res.data,
+                searchNum: res.data.length
+              });
+              console.log(res.data)
+            }
+          })
+
+      
+        //获取searchResult完毕
     },
     // onShow函数
     onShow: function() {
@@ -67,11 +84,13 @@ Page({
           url: '/pages/information/workingExperience/workingExperience?toupdate=' + toupdate,
         })
   },
-    _gotoWork:function(){
+    _gotoWork:function(e){
         var toupdate = true;
+        var toid = e.target.id;
         console.log(toupdate)
+        console.log(toid)
         wx.navigateTo({
-            url: '/pages/information/workingExperience/workingExperience?toupdate=' + toupdate,
+            url: '/pages/information/workingExperience/workingExperience?toupdate=' + toupdate + '&toid=' + toid,
         })
     },
     gotoEducation:function(){
