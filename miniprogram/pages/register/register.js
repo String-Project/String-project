9471841379c039f039d7b23b5d85b6be758b86e8
CSWellesSun <3200102339@zju.edu.cn>
2023-09-29 21:55:32 +0800

    fix: rename page

--- miniprogram/pages/register/register.js
+++ miniprogram/pages/register/register.js
@@ -3,8 +3,7 @@ const defaultAvatarUrl =
 
 Page({
   data: {
-    avatarFlag: false,
-    phoneFlag: false,
+    flag: 0,
     code: null,
 
     avatarUrl: defaultAvatarUrl,
@@ -51,54 +50,62 @@ Page({
   },
 
   next: function () {
-    this.setData({
-      phoneFlag: true,
+    let that = this;
+    wx.showModal({
+      title: "提示",
+      content: "本产品暂仅对弦计划内部成员开放欢迎关注弦计划获取纳新动态，如你为内部成员但无法进入请联系工作人员",
+      showCancel: false,
+      success: (res) => {
+        that.setData({
+          flag: 1,
+        });
+      },
     });
   },
 
-  getPhoneNumber(e) {
-    // 用户拒绝授权
-    if (e.detail.errMsg == "getPhoneNumber:fail user deny") {
-      wx.showToast({
-        icon: "none",
-        title: "请允许获取手机号，否则功能不可用！",
-      });
-      return;
-    }
-    console.log(e);
-    if (e.detail.code) {
-      console.log(e.detail.code);
-      wx.cloud
-        .callFunction({
-          name: "getPhoneNumber",
-          data: {
-            code: e.detail.code,
-          },
-        })
-        .then((res) => {
-          console.log(res);
-          if (res.errMsg == "cloud.callFunction:ok") {
-            let phoneNumber = res.result.phoneNumber;
-            this.setData({
-              phoneNumber: phoneNumber,
-              phoneFlag: true,
-            });
-          } else {
-            wx.showToast({
-              icon: "none",
-              title: "获取失败",
-            });
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-          wx.showToast({
-            icon: "none",
-            title: "获取失败",
-          });
-        });
-    }
-  },
+  // getPhoneNumber(e) {
+  //   // 用户拒绝授权
+  //   if (e.detail.errMsg == "getPhoneNumber:fail user deny") {
+  //     wx.showToast({
+  //       icon: "none",
+  //       title: "请允许获取手机号，否则功能不可用！",
+  //     });
+  //     return;
+  //   }
+  //   console.log(e);
+  //   if (e.detail.code) {
+  //     console.log(e.detail.code);
+  //     wx.cloud
+  //       .callFunction({
+  //         name: "getPhoneNumber",
+  //         data: {
+  //           code: e.detail.code,
+  //         },
+  //       })
+  //       .then((res) => {
+  //         console.log(res);
+  //         if (res.errMsg == "cloud.callFunction:ok") {
+  //           let phoneNumber = res.result.phoneNumber;
+  //           this.setData({
+  //             phoneNumber: phoneNumber,
+  //             phoneFlag: true,
+  //           });
+  //         } else {
+  //           wx.showToast({
+  //             icon: "none",
+  //             title: "获取失败",
+  //           });
+  //         }
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //         wx.showToast({
+  //           icon: "none",
+  //           title: "获取失败",
+  //         });
+  //       });
+  //   }
+  // },
 
   getUserProfile(e) {
     wx.getUserProfile({
@@ -109,7 +116,7 @@ Page({
         this.setData({
           avatarUrl: result.userInfo.avatarUrl,
           nickname: result.userInfo.nickName,
-          avatarFlag: true,
+          flag: 2
         });
       },
       fail: (res) => {
